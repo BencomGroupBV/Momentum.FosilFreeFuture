0d9e45627815086b6b3fc0402e1ab216436e3386
Sjoerd Heidinga <sjoerd@bencom.nl>
2022-06-09 20:35:04 +0200

    db back

--- Benchain.FosilFreeFuture.Web/Models/DbEntities/Helper/DbHelper.cs
+++ Benchain.FosilFreeFuture.Web/Models/DbEntities/Helper/DbHelper.cs
@@ -1,26 +1,67 @@
 ﻿using Benchain.FosilFreeFuture.Service.Models;
+using Benchain.FosilFreeFuture.Web.Models;
 using Benchain.FosilFreeFuture.Web.Models.DbEntities;
 
-namespace Benchain.FosilFreeFuture.Web
+namespace Benchain.FosilFreeFuture.Web;
+
+public static class DbHelper
 {
-  public static class DbHelper
+  public static ProjectModel ParseProjectDb(ProjectDb projectDb)
   {
-    public static ProjectModel ParseProjectDb(ProjectDb projectDb)
+    var model = new ProjectModel
     {
-      var model = new ProjectModel
-      {
-        Id = $"{projectDb.Id}",
-        Name = projectDb.Name,
-        Country = projectDb.Country,
-        Description = projectDb.Description,
-        Logo = projectDb.Logo,
-        Image = projectDb.Image,
-        InitiatorWalletAddress = projectDb.Initiated,
-        FundsNeeded = projectDb.FundsNeeded,
-        FundsReceived = projectDb.FundsReceived,
-        Status = projectDb.Status,
-      };
-      return model;
-    }
+      Id = $"{projectDb.Id}",
+      Name = projectDb.Name,
+      Country = projectDb.Country,
+      Description = projectDb.Description,
+      Logo = projectDb.Logo,
+      Image = projectDb.Image,
+      InitiatorWalletAddress = projectDb.Initiated,
+      FundsNeeded = projectDb.FundsNeeded,
+      FundsReceived = projectDb.FundsReceived,
+      Status = projectDb.Status,
+    };
+    return model;
+  }
+
+  public static ProfileModel ParsProfileDb(ProfileDb? profileDb)
+  {
+    var model = new ProfileModel
+    {
+      Id = profileDb.Id,
+      Name = profileDb.Name,
+      Type = profileDb.Type,
+      City = profileDb.City,
+      Country = profileDb.Country,
+      CurrentBalance = profileDb.CurrentBalance,
+      Avatar = profileDb.Avatar
+    };
+    return model;
+  }
+
+  public static PortfolioModel ParsPortfolioDb(PortfolioDb portfolioDb)
+  {
+    var model = new PortfolioModel()
+    {
+      Id = portfolioDb.Id,
+      Name = portfolioDb.Name,
+      Balance = portfolioDb.Balance,
+      ParticipantId = portfolioDb.ParticipantId,
+      ProfileId = portfolioDb.ProfileId
+    };
+    return model;
+  }
+
+  public static BadgeModel ParsBadgeDb(BadgeDb badgeDb)
+  {
+    var model = new BadgeModel()
+    {
+      Id = badgeDb.Id,
+      Name = badgeDb.Name,
+      Image = badgeDb.Image,
+      Subtitle = badgeDb.Subtitle
+    };
+    return model;
   }
 }
+
