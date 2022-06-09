bbab0158ffd40f20ef5297059108e8b5a3e6ca71
Jan-Willem Maris <jan-willem@bencom.nl>
2022-06-09 11:42:35 +0200

    WIP

--- Momentum.FosilFreeFuture.Service/Blockchain/contracts/ProjectFunding.sol
+++ Momentum.FosilFreeFuture.Service/Blockchain/contracts/ProjectFunding.sol
@@ -1,41 +1,7 @@
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
-// import '@openzeppelin/contracts/utils/math/SafeMath.sol';
-
-contract ProjectFunding {
-// using SafeMath for uint256;
-
- Project[] public projects;
-
-    event ProjectStarted(
-        address contractAddress,
-        address projectStarter,
-        string projectTitle,
-        string projectDesc,
-        uint256 goalAmount
-    );
-
-    function createProject(
-    string calldata title,
-    string calldata description,
-    uint amountToRaise
-
-    ) external {
-        Project newProject = new Project (payable(msg.sender), title, description, amountToRaise);
-        projects.push(newProject);
-        emit ProjectStarted(
-            address(newProject),
-            payable(msg.sender),
-            title,
-            description,
-            amountToRaise
-        );
-    }   
-
-}
-
 contract Project {
-    // using SafeMath for uint256;
     
     enum State {
         Fundraising,
@@ -64,18 +30,18 @@ contract Project {
 
     constructor
     (
-        string storage _name,
-        string storage _country,
-        string storage _description,
-        string storage _fundsNeeded,
-        string storage _image,
-        string storage _logo,
-        string storage _initiated,
-        string storage _status
+        string memory _name,
+        string memory _country,
+        string memory _description,
+        uint256 _fundsNeeded,
+        string memory _image,
+        string memory _logo,
+        string memory _initiated,
+        string memory _status
         // string memory projectTitle,
         // string memory projectDesc,
         // uint goalAmount
-    ) public {
+    ) {
         name = _name;
         country = _country;
         description = _description;
