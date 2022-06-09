6b3fd0d0c11fa4240c4dd989c27efcdc98fc667b
Jan-Willem Maris <jan-willem@bencom.nl>
2022-06-09 16:49:10 +0200

    Solidity get all projects

--- Momentum.FosilFreeFuture.Service/Blockchain/contracts/ProjectFunding.sol
+++ Momentum.FosilFreeFuture.Service/Blockchain/contracts/ProjectFunding.sol
@@ -1,17 +1,51 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity >=0.5.4 <0.9.0;
 
-contract Project {
-    
-    // enum State {
-    //     Fundraising,
-    //     Successful
-    // }
+contract ProjectFunding {
+    struct ProjectDetails {
+        string name;
+        string description;
+        string country;
+    }
 
-    // address payable public creator;
-    // uint public amountGoal; 
-    // uint256 public currentBalance;
-    
+    // List of existing projects
+    Project[] private projects;
+
+    event ProjectCreated(
+        address contractAddress
+    );
+
+    function createProject(
+        ProjectDetails memory details,
+        uint256 fundsNeeded,
+        string calldata image,
+        string calldata logo,
+        string calldata initiated,
+        string calldata status  // Enum?
+    ) external {
+        Project newProject = new Project(
+            details.name,
+            details.country,
+            details.description,
+            fundsNeeded,
+            image,
+            logo,
+            initiated,
+            status
+        );
+        projects.push(newProject);
+        emit ProjectCreated(
+            address(newProject)
+        );
+    }
+
+    function returnAllProjects() external view returns(Project[] memory){
+        return projects;
+    }
+
+}
+
+contract Project {    
     string public name;
     string public country;
     string public description;
@@ -20,9 +54,6 @@ contract Project {
     string public logo;
     string public initiated;
     string public status;
-
-    // State public state = State.Fundraising; 
-    // mapping (address => uint) public contributions;
 
     constructor
     (
@@ -47,22 +78,22 @@ contract Project {
 
     function GetProjectDetails() public view returns 
     (
-        string memory name_,
-        string memory country_,
-        string memory description_,
-        uint256 fundsNeeded_,
-        string memory image_,
-        string memory logo_,
-        string memory initiated_,
-        string memory status_
+        string memory projectName,
+        string memory projectCountry,
+        string memory projectDescription,
+        uint256 projectFundsNeeded,
+        string memory projectImage,
+        string memory projectLogo,
+        string memory projectInitiated,
+        string memory projectStatus
     ) {
-        name_ = name;
-        country_ = country;
-        description_ = description;
-        fundsNeeded_ = fundsNeeded;
-        image_ = image;
-        logo_ = logo;
-        initiated_ = initiated;
-        status_ = status;
+        projectName = name;
+        projectCountry = country;
+        projectDescription = description;
+        projectFundsNeeded = fundsNeeded;
+        projectImage = image;
+        projectLogo = logo;
+        projectInitiated = initiated;
+        projectStatus = status;
     }
 }
