e9fba46ad3db37f26760ac4357a2fca9b450a19d
Jeroen Maat <jeroen@bencom.nl>
2022-06-09 11:45:18 +0200

    Add getProjects()

--- Momentum.FosilFreeFuture.Service/ProjectSmartContractService.cs
+++ Momentum.FosilFreeFuture.Service/ProjectSmartContractService.cs
@@ -1,8 +1,10 @@
 ﻿using System.Numerics;
 using Benchain.FosilFreeFuture.Service.Interfaces;
+using Benchain.FosilFreeFuture.Service.Models;
 using Microsoft.Extensions.Configuration;
 using Nethereum.Contracts;
 using Nethereum.Hex.HexTypes;
+using Nethereum.RPC.Eth.DTOs;
 using Nethereum.Web3;
 
 namespace Benchain.FosilFreeFuture.Service
@@ -10,7 +12,7 @@ namespace Benchain.FosilFreeFuture.Service
   public class ProjectSmartContractService : IProjectSmartContractService
   {
     private readonly IConfiguration _config;
-    private const string address = "0x3581D61A8181bD531C013ddD4fC37D4E846f5e42"; //ProjectFunding.sol
+    private const string contractAddress = "0xd6c3CD4a8929d46a03ccA5fc32c9fB02217D41Eb"; //ProjectFunding.sol
     private const string ABI = @"[{'anonymous':false,'inputs':[{'indexed':false,'internalType':'address','name':'contractAddress','type':'address'},{'indexed':false,'internalType':'address','name':'projectStarter','type':'address'},{'indexed':false,'internalType':'string','name':'projectTitle','type':'string'},{'indexed':false,'internalType':'string','name':'projectDesc','type':'string'},{'indexed':false,'internalType':'uint256','name':'goalAmount','type':'uint256'}],'name':'ProjectStarted','type':'event'},{'inputs':[{'internalType':'uint256','name':'','type':'uint256'}],'name':'projects','outputs':[{'internalType':'contract Project','name':'','type':'address'}],'stateMutability':'view','type':'function'},{'inputs':[{'internalType':'string','name':'title','type':'string'},{'internalType':'string','name':'description','type':'string'},{'internalType':'uint256','name':'amountToRaise','type':'uint256'}],'name':'createProject','outputs':[],'stateMutability':'nonpayable','type':'function'}]";
 
 
@@ -22,7 +24,7 @@ namespace Benchain.FosilFreeFuture.Service
     private Contract GetProjectSmartContract()
     {
       var web3 = new Web3(_config["BlockchainNetwork:EndPoint"]);
-      var contract = web3.Eth.GetContract(ABI, address);
+      var contract = web3.Eth.GetContract(ABI, contractAddress);
 
       return contract;
     }
@@ -30,7 +32,7 @@ namespace Benchain.FosilFreeFuture.Service
     public string CreateProject()
     {
       var createProjectSmartContract = GetProjectSmartContract();
-      var projectCreatorAccountAddress = "0xF1C8D9F04cADE0BF3Be8f8f4afD27497023Dd82A";
+      var projectCreatorAccountAddress = "0xbD12e63fbC925DCA822d2Ad415E24d2E796fB353";
       var projectName = "Test Project";
       var projectDescription = "Test Description";
       var projectAmount = 50;
@@ -54,15 +56,20 @@ namespace Benchain.FosilFreeFuture.Service
 
     }
 
-    public Task<HexBigInteger> GetProjects()
+    public List<ProjectStartedEventDTO> GetProjects()
     {
       var createProjectSmartContract = GetProjectSmartContract();
-      var createProjectEvent = createProjectSmartContract.GetEvent("ProjectStarted");
-      var filterforAll = createProjectEvent.CreateFilterAsync();
 
-      filterforAll.Wait();
+      var eventLog = createProjectSmartContract.GetEvent("ProjectStarted");
+      var filterInput = eventLog.CreateFilterInput(BlockParameter.CreateEarliest(), BlockParameter.CreateLatest());
+      var logs = eventLog.GetAllChangesAsync<ProjectStartedEventDTO>(filterInput);
 
-      return filterforAll;
+      logs.Wait();
+
+      var results = new List<ProjectStartedEventDTO>();
+      logs.Result.ForEach(result => results.Add(result.Event));
+
+      return results;
     }
 
     public async Task<BigInteger> GetContractFunction(string functionName, Contract contract)
